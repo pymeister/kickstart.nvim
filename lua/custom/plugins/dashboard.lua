9b6759c2e766c1e67cc54a98fcb00761ae653caf
PyMeister <k.lancemeister@gmail.com>
2024-04-21 21:01:14 +0200

    nvim setup complete so far

---
+++ lua/custom/plugins/dashboard.lua
@@ -0,0 +1,108 @@
+return {
+  'nvimdev/dashboard-nvim',
+  event = 'VimEnter',
+  config = function()
+    require('dashboard').setup {
+      -- config
+      theme = 'doom', --  theme is doom and hyper default is hyper
+      disable_move = false, -- default is false disable move keymap for hyper
+      shortcut_type = 'letter', --  shorcut type 'letter' or 'number'
+      change_to_vcs_root = false, -- default is false,for open file in hyper mru. it will change to the root of vcs
+      config = {
+
+        header = {
+          '',
+          ' ⣿⣿⣷⡁⢆⠈⠕⢕⢂⢕⢂⢕⢂⢔⢂⢕⢄⠂⣂⠂⠆⢂⢕⢂⢕⢂⢕⢂⢕⢂ ',
+          ' ⣿⣿⣿⡷⠊⡢⡹⣦⡑⢂⢕⢂⢕⢂⢕⢂⠕⠔⠌⠝⠛⠶⠶⢶⣦⣄⢂⢕⢂⢕ ',
+          ' ⣿⣿⠏⣠⣾⣦⡐⢌⢿⣷⣦⣅⡑⠕⠡⠐⢿⠿⣛⠟⠛⠛⠛⠛⠡⢷⡈⢂⢕⢂ ',
+          ' ⠟⣡⣾⣿⣿⣿⣿⣦⣑⠝⢿⣿⣿⣿⣿⣿⡵⢁⣤⣶⣶⣿⢿⢿⢿⡟⢻⣤⢑⢂ ',
+          ' ⣾⣿⣿⡿⢟⣛⣻⣿⣿⣿⣦⣬⣙⣻⣿⣿⣷⣿⣿⢟⢝⢕⢕⢕⢕⢽⣿⣿⣷⣔ ',
+          ' ⣿⣿⠵⠚⠉⢀⣀⣀⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣗⢕⢕⢕⢕⢕⢕⣽⣿⣿⣿⣿ ',
+          ' ⢷⣂⣠⣴⣾⡿⡿⡻⡻⣿⣿⣴⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣵⣷⣿⣿⣿⣿⡿ ',
+          ' ⢌⠻⣿⡿⡫⡪⡪⡪⡪⣺⣿⣿⣿⣿⣿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃ ',
+          ' ⠣⡁⠹⡪⡪⡪⡪⣪⣾⣿⣿⣿⣿⠋⠐⢉⢍⢄⢌⠻⣿⣿⣿⣿⣿⣿⣿⣿⠏⠈ ',
+          ' ⡣⡘⢄⠙⣾⣾⣾⣿⣿⣿⣿⣿⣿⡀⢐⢕⢕⢕⢕⢕⢅⣿⣿⣿⣿⣿⣿⠏⠠⠈ ',
+          ' ⠌⢊⢂⢣⠹⣿⣿⣿⣿⣿⣿⣿⣿⣧⢐⢕⢕⢕⢕⢕⢅⣿⣿⣿⣿⡿⢋⢜⠠⠈ ',
+          ' ⠄⠁⠕⢝⡢⠈⠻⣿⣿⣿⣿⣿⣿⣿⣷⣕⣑⣑⣑⣵⣿⣿⣿⡿⢋⢔⢕⣿⠠⠈ ',
+          ' ⠨⡂⡀⢑⢕⡅⠂⠄⠉⠛⠻⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢋⢔⢕⢕⣿⣿⠠ ',
+          ' ⠄⠪⣂⠁⢕⠆⠄⠂⠄⠁⡀⠂⡀⠄⢈⠉⢍⢛⢛⢛⢋⢔⢕⢕⢕⣽⣿⣿⠠⠈ ',
+          '',
+          ' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
+          ' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
+          ' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
+          ' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
+          ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
+          ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
+          '',
+        },
+
+        center = {
+
+          {
+            icon = ' ',
+            icon_hl = 'Title',
+            desc = 'New File           ',
+            desc_hl = 'String',
+            key = 'f',
+            keymap = 'SPC n f',
+            key_hl = 'Number',
+            action = 'DashboardNewFile',
+          },
+          {
+            icon = ' ',
+            desc = 'Find File          ',
+            key = 'g',
+            keymap = 'SPC s f',
+            action = 'Telescope find_files',
+          },
+          {
+            icon = ' ',
+            desc = 'Find Word          ',
+            key = 'w',
+            keymap = 'SPC f w',
+            action = 'Telescope live_grep',
+          },
+          {
+            icon = ' ',
+            desc = 'Recent Files       ',
+            key = 'r',
+            keymap = 'SPC f r',
+            action = 'Telescope oldfiles',
+          },
+          {
+            icon = ' ',
+            desc = 'File Browser       ',
+            key = 'b',
+            keymap = 'SPC f b',
+            action = 'Telescope file_browser',
+          },
+          {
+            icon = ' ',
+            desc = 'Config             ',
+            key = 'c',
+            keymap = 'SPC f c',
+            action = 'edit ~/.config/nvim/init.lua',
+          },
+          {
+            icon = ' ',
+            desc = 'Quit               ',
+            key = 'q',
+            keymap = 'SPC q',
+            action = 'quit',
+          },
+        },
+        footer = {
+          '',
+          'Doom Emacs-style dashboard in Neovim',
+          'Press <Space> followed by the shortcut key to perform an action',
+        }, --your footer
+      },
+      hide = {
+        statusline = true, -- hide statusline default is true
+        tabline = true, -- hide the tabline
+        winbar = true, -- hide winbar
+      },
+    }
+  end,
+  dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+}
